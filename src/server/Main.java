409254218839529c4122ee9d879f4bf2e38bbdd5
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-03-15 14:29:37 +0100

    filling in the list from server side

--- src/server/Main.java
+++ src/server/Main.java
@@ -10,140 +10,56 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static final String address = "192.168.1.29";
-    private static final int port = 34522;
 
     public static void main(String[] args) {
         String[] listOfData = new String[100];
-        try (ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));) {
-            while (true) {
-                try (
-                        Socket socket = server.accept(); // accept a new client
-                        DataInputStream input = new DataInputStream(socket.getInputStream());
-                        DataOutputStream output = new DataOutputStream(socket.getOutputStream())
-                ) {
-                    while (true) {
-                        String msg = input.readUTF(); // read a message from the client
-                        String[] listOfMsg = msg.split("\\s");
-                        if (msg.equals("exit")) {
-                            return;
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\\n");
+        String msg;
+        while(true) {
+            msg = scanner.next();
+            String[] listOfMsg = msg.split("\\s");
+            if (msg.equals("exit")) {
+                return;
 
-                        } else if (listOfMsg[0].equals("get")) {
-                            int index = Integer.parseInt(msg.split("\\s")[1]);
+            } else if (listOfMsg[0].equals("get")) {
+                int index = Integer.parseInt(msg.split("\\s")[1]);
 
-                            if (index < 1 || index > 100 || listOfData[index - 1] == null) {
-                                output.writeUTF("ERROR");
-                            } else {
-                                output.writeUTF(listOfData[index - 1]);
+                if (index < 1 || index > 100 || listOfData[index - 1] == null) {
+                    System.out.println("ERROR");
+                } else {
+                    System.out.println(listOfData[index - 1]);
 
-/*                                List<database> results = deserializer();
+                }
 
-                                boolean isId = false;
-                                for (database object : results
-                                ) {
-                                    if (object.getIndex() == index - 1) {
-                                        isId = true;
-                                        output.writeUTF(object.getText());
-                                    }
-                                }
-                                if(!isId)
-                                {
-                                    output.writeUTF("ERROR");
-                                }*/
-                            }
-
-                        } else if (listOfMsg[0].equals("delete")) {
-                            int index = Integer.parseInt(listOfMsg[1]);
-                            if (index < 1 || index > 100 ) {
-                                output.writeUTF("ERROR");
-                            } else {
-                                listOfData[index - 1] = null;
-/*                                List<database> results = deserializer();
-                                for (database object : results
-                                ) {
-                                    if (object.getIndex() == index - 1) {
-                                        serializer(index - 1, results);
-                                    }
-                                }*/
-                                output.writeUTF("OK");
-                            }
-                        } else if (listOfMsg[0].equals("set")) {
-                            int index = Integer.parseInt(listOfMsg[1]);
-                            if (index < 1 || index > 100 ) {
-                                output.writeUTF("ERROR");
-                            } else {
-                                listOfData[index - 1] = "";
-                                for (int i = 2; i < listOfMsg.length; i++) {
-                                    listOfData[index - 1] += listOfMsg[i] + " ";
-                                }
-
-                                output.writeUTF("OK");
-/*                                database[index - 1] = "";
-                                for (int i = 2; i < listOfMsg.length; i++) {
-                                    database[index - 1] += listOfMsg[i] + " ";
-                                }
-
-                                database data = new database();
-                                data.setIndex(Integer.parseInt(listOfMsg[1]) - 1);
-                                data.setText(database[index - 1]);
-
-                                FileOutputStream fileOutputStream
-                                        = new FileOutputStream("data.txt", true);
-                                ObjectOutputStream objectOutputStream
-                                        = new ObjectOutputStream(fileOutputStream);
-                                objectOutputStream.writeObject(data);
-                                objectOutputStream.close();*/
-
-                            }
-                        }
+            } else if (listOfMsg[0].equals("delete")) {
+                int index = Integer.parseInt(listOfMsg[1]);
+                if (index < 1 || index > 100 ) {
+                    System.out.println("ERROR");
+                } else {
+                    listOfData[index - 1] = null;
+                    System.out.println("OK");
+                }
+            } else if (listOfMsg[0].equals("set")) {
+                int index = Integer.parseInt(listOfMsg[1]);
+                if (index < 1 || index > 100 ) {
+                    System.out.println("ERROR");
+                } else {
+                    listOfData[index - 1] = "";
+                    for (int i = 2; i < listOfMsg.length; i++) {
+                        listOfData[index - 1] += listOfMsg[i] + " ";
                     }
+                    System.out.println("OK");
 
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
     }
-
-    public static List<database> deserializer() throws IOException {
-        List<database> results = new ArrayList<>();
-
-        FileInputStream fis = null;
-        try {
-            fis =new FileInputStream("data.txt");
-            while (true) {
-                ObjectInputStream ois = new ObjectInputStream(fis);
-                results.add((database) ois.readObject());
-            }
-        } catch (EOFException | FileNotFoundException ignored) {
-
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (fis != null)
-                fis.close();
-        }
-
-        return results;
-    }
-    public static void serializer(int id, List<database> listOfOldContent) throws IOException {
-        List<database> listOfNewContent = listOfOldContent.stream().
-                filter(data -> data.getIndex() != id).toList();
-        new FileOutputStream("data.txt").close();
-        if (listOfNewContent.size() >= 1) {
-            for (database object : listOfNewContent
-            ) {
-                FileOutputStream fos = new FileOutputStream("data.txt", true);
-                BufferedOutputStream bos = new BufferedOutputStream(fos);//for speeding up the I/O operations
-                ObjectOutputStream oos = new ObjectOutputStream(bos);
-                oos.writeObject(object);
-                oos.close();
-            }
-        }
-    }
-
 }
+
+
+
 
 
 
