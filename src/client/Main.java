ab776280106482a8242c7f42a65fa08d670be954
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-03-17 16:55:52 +0100

    {wip}

--- src/client/Main.java
+++ src/client/Main.java
@@ -8,24 +8,24 @@ import java.util.Scanner;
 
 public class Main{
 
-    private static final String SERVER_ADDRESS = "127.0.0.1";//you can also set adress of other computer if the server is in another machine
+    private static final String SERVER_ADDRESS = "192.168.1.29";//you can also set adress of other computer if the server is in another machine
     private static final int SERVER_PORT = 34522;
     public static void main(String[] args) {
+        System.out.println("Client started!");
         try (
                 Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
                 DataInputStream input = new DataInputStream(socket.getInputStream());
                 DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
         ) {
-
-            String msg = "Client started!";
-            output.writeUTF(msg);
-            System.out.println(msg);
-            System.out.println("Sent: Give me a record # 12");
-            output.writeUTF("Sent: Give me a record # 12");
-            String receivedMsg2 = input.readUTF();
-            System.out.println("Received: A record # 12 was sent!");
-
-
+            Scanner scanner = new Scanner(System.in);
+            scanner.useDelimiter("\\n");
+            String option = scanner.next();
+            if (option.equals("exit")) {
+                output.writeUTF(option);
+            } else {
+                output.writeUTF(option);
+                System.out.println(input.readUTF());
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/server/Main.java
+++ src/server/Main.java
@@ -10,44 +10,81 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static final String address = "127.0.0.1";
+    private static final String address = "192.168.1.29";
     private static final int port = 34522;
 
-    public static void main(String[] args) {
-        System.out.println("Server started!");
-        try (ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));) {
-            while (true) {
-                try (
-                        Socket socket = server.accept(); // accept a new client
-                        DataInputStream input = new DataInputStream(socket.getInputStream());
-                        DataOutputStream output = new DataOutputStream(socket.getOutputStream())
-                ) {
-                    String msgnew = input.readUTF();
-                    System.out.println("Received: Give me a record # 12");
+    private  static volatile boolean isExit = true;
 
-                    /**
-                     * to accept one message
-                     */
-                    String msg = input.readUTF(); // read a message from the client
-                    output.writeUTF(msg); // resend it to the client
-                    System.out.println("Sent: A record # 12 was sent!");
-                    return;
+    static class Session extends Thread {
 
+        private String[] databaseArray;
+
+        private final Socket socket;
+
+        public Session(Socket socketForClient, String[] databaseArray) {
+
+            this.socket = socketForClient;
+            this.databaseArray = databaseArray;
+        }
+        public void run() {
+            try (
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+            ) {
+
+                String msg = input.readUTF(); // read a message from the client
+                String[] listOfMsg = msg.split("\\s");
+                if (msg.equals("exit")) {
+                    isExit = false;
+                } else if (msg.split("\\s")[0].equals("get")) {
+                    int index = Integer.parseInt(msg.split("\\s")[1]);
+
+                    if (index < 1 || index > 1000 || databaseArray[index - 1] == null) {
+                        output.writeUTF("ERROR");
+                    } else {
+                        output.writeUTF(databaseArray[index - 1]);
+                    }
+
+                } else if (msg.split("\\s")[0].equals("delete")) {
+                    int index = Integer.parseInt(msg.split("\\s")[1]);
+                    if (index < 1 || index > 1000 ) {
+                        output.writeUTF("ERROR");
+                    } else {
+                        databaseArray[index - 1] = null;
+                        output.writeUTF("OK");
+
+                    }
+                } else if (msg.split("\\s+")[0].equals("set")) {
+                    int index = Integer.parseInt(msg.split("\\s")[1]);
+                    if (index < 1 || index > 1000 ) {
+                        output.writeUTF("ERROR");
+                    } else {
+                        databaseArray[index - 1] = "";
+                        for (int i = 2; i < listOfMsg.length; i++) {
+                            databaseArray[index - 1] += listOfMsg[i] + " ";
+                        }
+                        output.writeUTF("OK");
+
+                    }
                 }
-
-
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
+
+
+    public static void main(String[] args) throws IOException {
+        String[] databaseArray = new String[1000];
+        System.out.println("Server started!");
+        
+        try (ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));) {
+            while (isExit) {
+                Session session = new Session(server.accept(), databaseArray);
+                session.start(); // does not block this server thread
+            }
+        }
+
+    }
 }
-
-
-
-
-
-
-
-
-
