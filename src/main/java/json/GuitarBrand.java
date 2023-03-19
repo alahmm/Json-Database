276a8f41370cd66bef6183e1e9a57bfae0e740f6
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-03-19 08:14:40 +0100

    adding maven to build project and Gson library

--- src/main/java/json/GuitarBrand.java
+++ src/main/java/json/GuitarBrand.java
@@ -1,2 +1,40 @@
-package json;public class GuitarBrand {
+package json;
+
+import com.google.gson.Gson;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+class Main {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+        GuitarBrand espGuitar = new GuitarBrand(sdf.parse("03-08-1975"),
+                "ESP Guitars",
+                "Japan",
+                Arrays.asList("Metallica",
+                        "Children of Bodom",
+                        "The Rolling Stones", "Rammstein"));
+
+        String espGuitarJson = new Gson().toJson(espGuitar);
+        System.out.println(espGuitarJson);
+    }
+}
+public class GuitarBrand {
+    Date dateFounded;
+    String name;
+    String country;
+    List<String> artistsUsedBy;
+
+    public GuitarBrand(Date dateFounded, String name, String country, List<String> artistsUsedBy) {
+        this.artistsUsedBy = artistsUsedBy;
+        this.country = country;
+        this.name = name;
+        this.dateFounded = dateFounded;
+    }
+
+    // getters, setters, contructor
 }
